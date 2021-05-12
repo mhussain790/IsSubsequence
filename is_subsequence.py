68c462df2273b876af3f1e8b2f524e19b7961367
Masud Hussain <mhussain790@gmail.com>
2021-05-12 01:14:12 -0700

    Added docstring and removed test code.

--- is_subsequence.py
+++ is_subsequence.py
@@ -5,68 +5,39 @@ Assignment: 6C
 """
 
 
-# def is_subsequence(string_1, string_2):
-#     list_1 = string_1
-#     list_2 = string_2
-#
-#     list_length_1 = len(string_1)
-#     list_length_2 = len(string_2)
-#
-#     if list_length_1 == 0 and list_length_2 == 0:
-#         return False
-#     # elif list_length_1 >= 0 and list_length_2 == 0:
-#     #     print(True)
-#     #     return True
-#     # # if list_length_1 == 0 and list_length_2 >= 0:
-#     # #     new_string_2 = string_2[1:list_length_2:]
-#     # #     is_subsequence(list_1, new_string_2)
-#     # # el
-#
-#     if string_1[:0] == string_2[:0]:
-#         print(string_1)
-#         print(string_2)
-#
-#         new_string_1 = string_1[1:list_length_1:]
-#         new_string_2 = string_2[1:list_length_2:]
-#
-#         print(True)
-#         return True
-#     elif string_1[:0] != string_2[:0]:
-#         list_length_1 = len(string_1)
-#         list_length_2 = len(string_2)
-#
-#         new_string_1 = string_1[1:list_length_1:]
-#
-#         print(string_1)
-#         print(string_2)
-#
-#         is_subsequence(new_string_1, string_2)
-#
-#
-# is_subsequence("hen", "marj")
-
 def is_subsequence(string_1, string_2, count=0):
+    """
+    Method that takes two strings and a count variable, and returns a boolean value indicating if string_2 is a subsequence of string_1
+    :param string_1:
+    :param string_2:
+    :param count:
+    :return: True or False
+    """
+
+    # split up each character from the strings into respective lists
     list_1 = []
     list_1[:0] = string_1
-    print(list_1)
+    # print(list_1)
 
     list_2 = []
     list_2[:0] = string_2
-    print(list_2)
+    # print(list_2)
 
+    # Store the length of each list
     list_length_1 = len(string_1)
     list_length_2 = len(string_2)
 
+    # Base case
     if list_length_1 == 0 or list_length_2 == 0:
-        print(False)
+        # print(False)
         return False
     if count < list_length_2:
         if list_2[count] in list_1:
-            print(list_2[count])
-            print(True)
+            # print(list_2[count])
+            # print(True)
             return True
         else:
             return is_subsequence(string_1, string_2, count + 1)
     else:
-        print(False)
+        # print(False)
         return False
